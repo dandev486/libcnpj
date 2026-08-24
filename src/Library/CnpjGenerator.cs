cba103d1e4f8b19aff958d2ef1a74730643f24dd
dandev486 <dandev486@gmail.com>
2026-08-24 15:13:37 -0300

    class skeleton

---
+++ src/Library/CnpjGenerator.cs
@@ -0,0 +1,23 @@
+﻿/*
+LibCnpj, a simple library for validating Brazil's CNPJ.
+Copyright (C) 2026 Daniel Augusto
+SPDX-License-Identifier: LGPL-2.0-only or BSD-3-Clause (at your choice)
+*/
+namespace LibCnpj;
+
+public interface ICnpjGenerator
+{
+    string Generate();
+}
+
+public class CnpjGenerator : ICnpjGenerator
+{
+    public CnpjGenerator(CnpjValidationFormat format, bool withSeparators = true)
+    {
+    }
+
+    public string Generate()
+    {
+        throw new NotImplementedException();
+    }
+}
